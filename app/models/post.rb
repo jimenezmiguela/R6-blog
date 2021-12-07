8a124e89a8261826a7b4944b5a305cb5b87ab169
jimenezmiguela <jimenezmiguela@yahoo.com>
2021-12-07 15:28:08 -0800

    Work on Lesson 3.2, R6-blog, Associations completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord#subclass of ApplicationRecord class
+  has_many :comments #will add method to all post model objects with same name as symbol
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :pages
+  resources :posts do
+    resources :comments
+  end
 
+  # get '/post/:post_id/comments', to: 'comments#index', as: 'post_comments'
+  # get '/post/:post_id/comments', to: 'comments#create'
+  # get '/post/:post_id/comments/new', to: 'comments#new', as: 'new_post_comment'
+  # get '/post/:post_id/comments/:id', to: 'comments#show', as: 'post_comment'
+  # get '/post/:post_id/comments/:id/edit', to: 'comments#edit', as: 'edit_post_comment'
+  # get '/post/:post_id/comments/:id', to: 'comments#update'
+  # get '/post/:post_id/comments/:id', to: 'comments#destroy'
+
+  # get '/comments', to: 'comments#index', as: 'comments'
+  # get '/comments', to: 'comments#create'
+  # get '/comments/new', to: 'comments#new', as: 'new_comment'
+  # get '/comments/:id', to: 'comments#show', as: 'comment'
+  # get '/comments/:id/edit', to: 'comments#edit', as: 'edit_comment'
+  # get '/comments/:id', to: 'comments#update'
+  # get '/comments/:id', to: 'comments#destroy'
+
+  resources :pages
 
   # get '/pages', to: 'pages#index'
   # post '/pages', to: 'pages#create'
